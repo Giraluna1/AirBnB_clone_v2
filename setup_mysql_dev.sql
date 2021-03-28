c1c0965f359e19a63966bfbe60f77a5d7e3c540e
Giraluna1 <lunagolo@hotmail.com>
2021-03-28 18:46:41 -0500

    Fix case USEr  if not exist

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,7 +7,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 -- crear un nuevo usuario
 
-CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Darle los provilegios
 
