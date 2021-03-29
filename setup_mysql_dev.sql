68872a6cada64f2f2a0ad388b407849051d97c31
Giraluna1 <lunagolo@hotmail.com>
2021-03-28 19:27:55 -0500

    change perfomance

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,21 @@
 -- Prepares a MySQL server
 -- for the project
+--Creamos la DB
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+--crear un nuevo usuario
+
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbhn_dv_pwd';
+
+-- Darle los provilegios
+
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performace_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Darle el permiso de ver
+
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+--Actualizar los permisos
+
 FLUSH PRIVILEGES;
