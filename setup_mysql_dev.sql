0d6b611e71bf7d420b7ef00938025810de86dd32
Giraluna1 <lunagolo@hotmail.com>
2021-03-28 19:33:01 -0500

    Go back

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,20 +1,8 @@
 -- Prepares a MySQL server for the project
---Creamos la DB
-
+-- Prepares a MySQL server
+-- for the project
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
---crear un nuevo usuario
-
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
--- Darle los provilegios
-
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
--- Darle el permiso de ver
-
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-
---Actualizar los permisos
-
 FLUSH PRIVILEGES;
