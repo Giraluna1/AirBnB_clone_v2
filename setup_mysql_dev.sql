04749fbef296302201d43d5d282a3b9ff831dd37
Giraluna1 <lunagolo@hotmail.com>
2021-03-28 19:08:08 -0500

    Delete the backtics

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -11,9 +11,9 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Darle los provilegios
 
-GRANT ALL PRIVILEGES ON  `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON  hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- Darle el permiso de ver
 
-GRANT SELECT ON `performace_schema`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performace_schema.* TO 'hbnb_dev'@'localhost';
 
