d90e040a70736a66f06fc86eda99d9658f353eb6
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-03-29 18:13:16 -0500

    class inherints from Basemodel and Base

--- models/state.py
+++ models/state.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    tablename = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref="states")
+
+    # For FileStorage
+    @property
+    def cities(self):
+        from models import storage
+        # cual instancia de City tien
+        city_list = []
+        for key, value in storage.all('City').items():
+            if value.state_id == self.id:
+                city_list.append(value)
+        return city_list
