130f250785733a3557dc3d2b1a8c921b6e1b480f
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2021-03-30 13:57:39 -0500

    file update table import

--- models/place.py
+++ models/place.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
+from models.amenity import Amenity
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from os import getenv
 from sqlalchemy.orm import relationship
+
+
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('amenity_id', String(60), ForeignKey(
+                          'amenities.id'), nullable=False, primary_key=True),
+                      Column('place_id', String(60), ForeignKey(
+                          'places.id'), nullable=False, primary_key=True)
+                      )
 
 
 class Place(BaseModel, Base):
@@ -21,6 +30,12 @@ class Place(BaseModel, Base):
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
         reviews = relationship('Review', cascade='delete')
+        amenities = relationship(
+            'Amenity',
+            secondary=place_amenity,
+            back_populates='place_amenities',
+            viewonly=False
+        )
 
     else:
         city_id = ""
@@ -44,3 +59,19 @@ class Place(BaseModel, Base):
                 if value.place_id == self.id:
                     review_list.append(value)
             return review_list
+
+        @property
+        def amenities(self):
+            """returns a list of amenities"""
+            from models import storage
+            amenities_list = []
+            for key, value in storage.all('Amenity').items():
+                if value.place_amenity.place_id == self.id:
+                    amenities_list.append(value)
+            return amenities_list
+
+        @amenities.setter
+        def amenities(self, amenities):
+            """return"""
+            if place_amenity.place_id in amenities:
+                self.amenity_ids.append(place_amenity.amenity_id)
