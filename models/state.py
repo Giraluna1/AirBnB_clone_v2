f9689963ba1ce9d5cdd8aa71c11a4da6e5ee7021
Giraluna1 <lunagolo@hotmail.com>
2021-03-29 22:37:25 -0500

    Add case env = DB

--- models/state.py
+++ models/state.py
@@ -3,22 +3,28 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', backref="states", cascade="delete")
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        # Relationship con las ciudades en un DB
+        cities = relationship('City', backref="states", cascade="delete")
 
-    # For FileStorage
-    @property
-    def cities(self):
-        """method definition for cities"""
-        from models import storage
-        # cual instancia de City tien
-        city_list = []
-        for key, value in storage.all('City').items():
-            if value.state_id == self.id:
-                city_list.append(value)
-        return city_list
+    else:
+        name = ""
+        # La relacion en el FileStorage
+
+        @property
+        def cities(self):
+            """method definition for cities"""
+            from models import storage
+            # cual instancia de City tien
+            city_list = []
+            for key, value in storage.all('City').items():
+                if value.state_id == self.id:
+                    city_list.append(value)
+            return city_list
