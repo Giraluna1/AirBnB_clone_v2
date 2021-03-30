3297b7ae6198a120f24ff57af490a1d4b20438b6
Giraluna1 <lunagolo@hotmail.com>
2021-03-30 14:20:39 -0500

    Delete import place_amenity

--- models/amenity.py
+++ models/amenity.py
@@ -3,7 +3,6 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Table, ForeignKey
 from sqlalchemy.orm import relationship
-from models import place_amenity
 from os import getenv
 
 
@@ -13,7 +12,7 @@ class Amenity(BaseModel, Base):
         name = Column(String(128), nullable=False)
         place_amenities = relationship(
             'Place',
-            secondary=place_amenity,
+            secondary='place_amenity',
             back_populates='places',
             cascade='delete'
         )
