affe0d10a6df0ab9cc6b76c865fc475cb00cfea6
Giraluna1 <lunagolo@hotmail.com>
2021-03-29 21:16:46 -0500

    fix2

--- models/state.py
+++ models/state.py
@@ -9,7 +9,7 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship('City', backref="states")
+    cities = relationship('City', backref="states", cascade="delete")
 
     # For FileStorage
     @property
