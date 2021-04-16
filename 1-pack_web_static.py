cd70a502bcff782b4073cfc016f495d934ef62aa
Giraluna1 <lunagolo@hotmail.com>
2021-04-16 13:25:20 -0500

    Add script

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Module Compress before sendig """
+
+import fabric.api
+from fabric.api import local
+import datetime
+from os.path import getsize
+
+
+def do_pack():
+    local('mkdir -p versions')
+    # create archive
+    date = datetime.datetime.now()
+    date = date.strftime("%Y%m%d%H%M%S")
+    try:
+        local('tar -cvzf versions/web_static_{}.tgz web_static'.format(date))
+        total_size = getsize('./versions/web_static_{}.tgz'.format(date))
+        print('web_static packed: versions/web_static_{}.tgz -> {}Bytes'.format(date, total_size))
+        return ('./versions/web_static_{}.tgz'.format(date))
+    except Exception:
+        return None
