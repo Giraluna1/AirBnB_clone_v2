604fe2d5aa62abec007f1313979445d418249f39
Giraluna1 <lunagolo@hotmail.com>
2021-05-02 22:42:14 -0500

    Add other serverpage

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" This script starts a Flask web application """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    " method response page1"
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def home2():
+    " method response page2"
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=5000)
