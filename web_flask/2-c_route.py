1d8536d7eb2b83f908fdc265d343b7eb9ace3b4e
Giraluna1 <lunagolo@hotmail.com>
2021-05-02 22:56:04 -0500

    Add argument

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" This script starts a Flask web application """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    " method response page1"
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def home2():
+    " method response page2"
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def home3(text=None):
+    "This methos pass an argument"
+    return "C {}".format(text)
+
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=5000)
