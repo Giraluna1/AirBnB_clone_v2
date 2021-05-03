617e69858f7875ccb7209dc6ecc9bfc99b70b12c
Giraluna1 <lunagolo@hotmail.com>
2021-05-03 01:24:40 -0500

    update list

--- models/state.py
+++ models/state.py
@@ -14,17 +14,12 @@ class State(BaseModel, Base):
         # Relationship con las ciudades en un DB
         cities = relationship('City', backref="states", cascade="delete")
 
-    else:
-        name = ""
-        # La relacion en el FileStorage
-
-        @property
-        def cities(self):
-            """method definition for cities"""
-            from models import storage
-            # cual instancia de City tien
-            city_list = []
-            for key, value in storage.all('City').items():
-                if value.state_id == self.id:
-                    city_list.append(value)
-            return city_list
+    @property
+    def cities(self):
+        """method definition for cities"""
+        from models import storage
+        city_list = []
+        for key, value in storage.all('City').items():
+            if value.state_id == self.id:
+                city_list.append(value)
+        return city_list
