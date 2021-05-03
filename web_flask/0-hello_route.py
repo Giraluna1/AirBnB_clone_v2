7b7bc27b57a69c4a485cfee771c78b23f24de37a
Giraluna1 <lunagolo@hotmail.com>
2021-05-02 22:32:01 -0500

    add app

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-""" """
+""" This script starts a Flask web application """
 from flask import Flask
 
 app = Flask(__name__)
-strict_slashes = False
 
 
 @app.route('/')
 def home():
+    """ home method"""
     return "Hello HBNB!"
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run()
